503ae2b57d284a5e55d8c5ac255ead65b1bf5267
deebyrne26 <deebyrne26@gmail.com>
2025-10-21 21:17:29 -0400

    Fix useEffect dependency - remove confidenceThreshold reference

--- frontend/shared/hooks/useCorrelations.js
+++ frontend/shared/hooks/useCorrelations.js
@@ -29,7 +29,7 @@ export const useCorrelations = (timeframeDays = 180) => {
 
   useEffect(() => {
     fetchCorrelations();
-  }, [confidenceThreshold, timeframeDays]);
+  }, [timeframeDays]);
 
   const retryFetch = () => {
     fetchCorrelations();
